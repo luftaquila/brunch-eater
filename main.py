de3eb6d50c2af842aea318e6e9d37445f83fb0f5
luftaquila <mail@luftaquila.io>
2020-11-01 17:58:29 +0900

    sorting output keywords by appearance

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ import time
 import json
 import getopt
 import datetime
+import requests
 import selenium
 import threading
-import configparser
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from urllib import request, parse
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
@@ -22,6 +20,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
+DATA = { 'count': 0, 'data': [ ], 'keyword': { } }
 
 def main(argv):
   # Parsing options
@@ -69,21 +68,86 @@ def main(argv):
     driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=chrome_options)
   except selenium.common.exceptions.WebDriverException as e:
     print(Fore.RED + 'fail\n' + str(e))
-    sys.exit()
+    sys.exit(1)
   print(Fore.GREEN + 'OK')
   sys.stdout.flush()
   
   
-  # Start scanning keywords
+  # Initializing output file
+  with open(OUTPUT if OUTPUT else 'output.json', "w") as f:
+    f.write(json.dumps(DATA, ensure_ascii = False))
+    
+  
+  # Starting keywords scan
   if isinstance(KEYWORD, str): # if KEYWORD is a single string
-    keyword_scan(KEYWORD)
+    keyword_scan(KEYWORD, SCAN_NUMBER, OUTPUT, driver)
   else: # if KEYWORD is a keyword array
     for keyword in KEYWORD:
-      keyword_scan(keyword)
-
+      keyword_scan(keyword, SCAN_NUMBER, OUTPUT, driver)
+  
+  
+  # Keyword scan complete. Starting article scan
+  print(Fore.GREEN + 'Keyword scan complete. Collected: ' + str(DATA['count']) + '\n')
+  print(Fore.RESET + 'Scanning collected articles... #0 / ' + str(DATA['count']), end='')
+  article_scan_count = 0
+  
+  for article in DATA['data']:
+    article['keyword'] = [ ]
+    # Loading target article
+    try:
+      #print('https://brunch.co.kr/@' + str(article['profileId']) + '/' + str(article['no']))
+      driver.get('https://brunch.co.kr/@' + str(article['profileId']) + '/' + str(article['no']))
+      WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h1.cover_title')))
+    except selenium.common.exceptions.TimeoutException as e: 
+      print(Fore.RED + '\nFAIL:' + str(e))
+      sys.exit(1)
       
-def keyword_scan(KEYWORD):
-  print(Fore.RESET + 'Scanning keyword: ' + KEYWORD)
+    # Do some analysis on article
+    
+    # Analysis on keywords list
+    keywords = driver.find_element(By.CSS_SELECTOR, 'ul.list_keyword').find_elements(By.TAG_NAME, 'li')
+    for keyword in keywords:
+      target = keyword.find_element(By.TAG_NAME, 'a').get_attribute('innerText')
+      
+      # Update each data's keyword list
+      article['keyword'].append(target)
+      
+      # Update DATA['keyword']
+      if target in DATA['keyword']: DATA['keyword'][target] = DATA['keyword'][target] + 1
+      else: DATA['keyword'][target] = 1
+        
+    # Writing into file
+    with open(OUTPUT if OUTPUT else 'output.json', "w") as f:
+      f.write(json.dumps(DATA, ensure_ascii = False))
+
+    # Updating counter
+    article_scan_count = article_scan_count + 1
+    for i in range(len(str(article_scan_count - 1)) + 3 + len(str(DATA['count']))): print('\b', end='')
+    print(str(article_scan_count) + ' / ' + str(DATA['count']), end='')
+    sys.stdout.flush()
+    
+  print(Fore.RESET + '\nSorting keywords by appearance... ', end='')
+  DATA['keyword'] = { k: v for k, v in sorted(DATA['keyword'].items(), key=lambda x: x[1], reverse=True) }
+  print(DATA['keyword'])
+  sys.stdout.flush()
+  DATA['keyword'] = sorted(DATA['keyword'].items(), key=lambda x: x[1], reverse=True)
+  print(DATA['keyword'])
+  sys.stdout.flush()
+  print(Fore.GREEN + 'OK')
+  sys.stdout.flush()
+    
+  print(Fore.RESET + 'Performing finial data writing... ', end='')
+  with open(OUTPUT if OUTPUT else 'output.json', "w") as f:
+    f.write(json.dumps(DATA, ensure_ascii = False))
+    print(Fore.GREEN + 'OK')
+    sys.stdout.flush()
+
+  print(Fore.GREEN + 'Scan finished.')
+  
+
+def keyword_scan(KEYWORD, SCAN_NUMBER, OUTPUT, driver):
+  keyword_scan_count = 0
+  print(Fore.RESET + '\nScanning keyword: ' + KEYWORD)
   sys.stdout.flush()
 
   # Loading keyword page
@@ -95,100 +159,99 @@ def keyword_scan(KEYWORD):
     WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'ul.list_article.list_common')))
   except selenium.common.exceptions.TimeoutException as e: 
     print(Fore.RED + 'fail\n' + str(e))
-    sys.exit()
+    sys.exit(1)
   print(Fore.GREEN + 'OK')
   sys.stdout.flush()
 
-  # Scan list of articles until maximum scan number met
-  if not SCAN_NUMBER: # if no maximum scan number is set
-    while True: # infinite loop. Break on moreList == false
-      break
-  else: # if there is a maximum scan number
-    while article_count < SCAN_NUMBER:
-      break
-      
-  time.sleep(3) # Wait until all articles are loaded. Nominal article count value is 20
-  article_count = len(driver.find_element(By.CSS_SELECTOR, 'ul.list_article.list_common').find_elements(By.TAG_NAME, 'li'))
-  print(article_count)
   # Looking for <script> tag with B.Keyword.init()
+  print(Fore.RESET + '  Locating initialization script... ', end='')
+  sys.stdout.flush()
+  script_flag = False
   for script_content in driver.find_element(By.TAG_NAME, 'body').find_elements(By.TAG_NAME, 'script'):
     script = script_content.get_attribute('innerHTML').strip()
-    #if script[:14] == 'B.Keyword.init':
-
-
+    if script[:14] == 'B.Keyword.init':
+      script_flag = script
       
-      
-      
-      
-      
-
-def crawler(driver, config, code, timeline):
-  # Crawling item info
-  print(Fore.RESET + 'Crawling item info... ', end='')
-  sys.stdout.flush()
-  try:
-    driver.get('https://www.ros-bot.com/user/' + code + '/bot-activity')
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "edit-item-destination")))
-    Select(driver.find_element(By.ID, 'edit-item-destination')).select_by_visible_text('Stashed')
-    driver.find_element(By.ID, 'edit-item-destination').submit()
-    # Getting timeline points
-    timepoints = driver.find_elements(By.CSS_SELECTOR, 'div.timeline-item div.row')
+  if script_flag:
     print(Fore.GREEN + 'OK')
+    print(Fore.RESET + '  Parsing initialization script... ', end='')
     sys.stdout.flush()
-
-    for point in timepoints:
-      soup = BeautifulSoup(point.get_attribute('innerHTML'), 'html.parser')
-      elem = soup.select_one('div.date').findAll(text=True, recursive=False)
-      date = None
-      # Identifying timeline point
-      for el in elem:
-        date = re.sub('\n', '', el).strip()
-        if date: break
-      # Getting timeline detail if new timeline appears
-      if not timeline.issuperset({ date }):
-        payload = { 'destination': config['user']['telegram'], 'date': date, 'contents': [] }
-        timeline.add(date)
-        items = soup.select_one('div.content').select('div p span')
-        for item in items:
-          payload['contents'].append({ 'title': item['data-title'], 'detail': re.sub('<br />\n', '\n', item['data-content']) })
-          
-        # Sending data to server
-        print(Fore.RESET + 'Transmitting data... ', end='')
-        sys.stdout.flush()
-        try:
-          req = request.Request('https://luftaquila.io/api/telepath/report', data=parse.urlencode(payload).encode())
-          res = request.urlopen(req)
-        except urllib.error.HTTPError as e:
-          print(Fore.RED + 'fail')
-          print(e)
-          continue
-        print(Fore.GREEN + 'OK')
-        sys.stdout.flush()
-        
-  except:
-    print(Fore.RED + 'fail')
-    print(sys.exc_info()[0])
-    sys.exit()
     
-  try:
-    target_time = datetime.datetime.now() + datetime.timedelta(0, int(config['rover']['refresh']))
-    print(Fore.RESET + 'Waiting for next execution: ', end='')
-    print(Fore.CYAN + str(target_time))
-    while(target_time - datetime.datetime.now()).total_seconds() > 0 :
-      print(Fore.RESET + '  Countdown: ', end='')
-      print(Fore.CYAN + str(target_time - datetime.datetime.now()), end='\r')
+    try:
+      object = script_flag[15:-17]
+      timestamp = script_flag[-15:-2]
+      object = object.replace('moreList', '"moreList"', 1).replace('keywordType', '"keywordType"', 1).replace("'single'", '"single"', 1).replace('articleList', '"articleList"', 1)
+      object = json.loads(object)
+      print(Fore.GREEN + 'OK')
       sys.stdout.flush()
-
-    print(Fore.RESET + '  Countdown: ', end='')
-    print(Fore.CYAN + '0:00:00.000000\n')
-    sys.stdout.flush()
-  except:
-    print(Fore.RED + 'fail')
-    print(sys.exc_info()[0])
-    sys.exit()
+      
+    except:
+      print(Fore.RED + 'fail')
+      print(sys.exc_info()[0])
+      sys.exit(1)
     
-  crawler(driver, config, code, timeline)
-  #threading.Timer(int(config['rover']['refresh']), crawler, [driver, config, code, timeline]).start()
+    # Scan list of articles until maximum scan number met
+    print(Fore.RESET + '  Parsing JSON object... #', end='')
+    print(keyword_scan_count, end='')
+    sys.stdout.flush()
+    
+    # scan first B.Keyword.init lists
+    for article in object['articleList']:
+      DATA['data'].append({
+        'likeCount': article['article']['likeCount'],
+        'title': article['article']['title'],
+        'readSeconds': article['article']['readSeconds'],
+        'userId': article['article']['userId'],
+        'profileId': article['article']['profileId'],
+        'no': article['article']['no']
+      })
+      DATA['count'] = DATA['count'] + 1
+      keyword_scan_count = keyword_scan_count + 1
+      for i in range(len(str(keyword_scan_count - 1))): print('\b', end='')
+      print(keyword_scan_count, end='')
+      sys.stdout.flush()
+            
+    while True: # lazyloading contents
+      timestamp, keyword_scan_count = scan(KEYWORD, SCAN_NUMBER, timestamp, keyword_scan_count)
+      if not timestamp: break
+    print()
+    
+  else: # Parsing initialization script failed
+    print(Fore.RED + 'fail')
+    print('Keyword scan failed. KEYWORD: ' + KEYWORD)
+    sys.stdout.flush()
+    return
+
+  
+def scan(KEYWORD, SCAN_NUMBER, timestamp, keyword_scan_count):
+  URL = 'https://api.brunch.co.kr/v1/top/keyword/' + KEYWORD
+  params = { 'publishTime': timestamp }
+  headers = { 'Sec-Fetch-Mode': 'cors', 'Accept': 'application/json, text/javascript, */*; q=0.01' }
+  response = requests.get(URL, params=params, headers=headers)
+  response = response.json()
+
+  for article in response['data']['articleList']:
+    DATA['data'].append({
+      'likeCount': article['article']['likeCount'],
+      'title': article['article']['title'],
+      'readSeconds': article['article']['readSeconds'],
+      'userId': article['article']['userId'],
+      'profileId': article['article']['profileId'],
+      'no': article['article']['no']
+    })
+    timestamp = article['timestamp']
+    DATA['count'] = DATA['count'] + 1
+    keyword_scan_count = keyword_scan_count + 1
+    for i in range(len(str(keyword_scan_count - 1))): print('\b', end='')
+    print(keyword_scan_count, end='')
+    sys.stdout.flush()
+
+    if SCAN_NUMBER:
+      if keyword_scan_count >= SCAN_NUMBER:
+        return False, keyword_scan_count # Break when SCAN_NUMBER met
+
+  if not response['data']['moreList']: return False, keyword_scan_count # Break when moreList is False
+  return timestamp, keyword_scan_count # continue scanning, passing last timestamp parameter
 
 if __name__ == "__main__":
   main(sys.argv)
